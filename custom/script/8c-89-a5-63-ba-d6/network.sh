d722b7db6e710a957246a756ffd0edfae647f8d7
Thomas Merkel <tm@core.io>
2013-11-11 21:05:17 +0100

    change config for network

--- custom/script/8c-89-a5-63-ba-d6/network.sh
+++ custom/script/8c-89-a5-63-ba-d6/network.sh
@@ -1,5 +1,7 @@
 #!/bin/ksh
 
+hostname=${1}
+
 ## Disable DAD for Solaris
 ## http://wiki.hetzner.de/index.php/Solaris_DAD 
 # IPv4
@@ -28,19 +30,14 @@ routeadm -u -e ipv4-forwarding
 # enable ipv6 forwarding
 routeadm -u -e ipv6-forwarding
 # setup nat rules
-cp /opt/custom/conf/ipf.conf /etc/ipf/ipf.conf
-cp /opt/custom/conf/ipnat.conf /etc/ipf/ipnat.conf
+cp /opt/custom/conf/${hostname}/ipf.conf /etc/ipf/ipf.conf
+cp /opt/custom/conf/${hostname}/ipnat.conf /etc/ipf/ipnat.conf
 # enable ipfilter
 svcadm enable network/ipfilter
 # reload
 #ipf -Fa -f /etc/ipf/ipf.conf
 #ipnat -C -f /etc/ipf/ipnat.conf
 
-## Global zone setup - additional ip
-#dladm create-vnic -l rge0 backup0
-#ifconfig backup0 plumb
-#ifconfig backup0 inet 176.9.28.121 netmask 255.255.255.224 up
-
 ## Copy svc methods
 # openvpn start script
-cp /opt/custom/bin/openvpn /lib/svc/method/openvpn
+cp /opt/custom/script/${hostname}/openvpn /lib/svc/method/openvpn
